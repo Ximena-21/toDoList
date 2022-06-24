0514b0a34e91865149c29210cca71cf95ca91ae4
Ximena-21 <ximenanzola.21@gmail.com>
2022-06-23 20:26:51 -0400

    trabajando en la funcion para renderizar los botones generales

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ const btnAdd = document.querySelector('.buttonAdd');
 const containerTasks = document.querySelector('.containerTasks');
 
 
-//funcion rendericeTarea, esta solo me ingrese la la tarea la caja 'task' dentro del 'boxTask'
-//que solo cuando haya una tarea ingresada se inserten lso bottones de selecccionar Todo y eliminar todo
 
-
+//funcion que me agregue la tarea ingresada por el usuario
 function addTask () {
     
     const task = document.querySelector('.inputTask').value;
     
+    //condicional, cuando el input este vacio no se ejecute funcion, sino 
+    //envie un alert al ususario 
     if (task === '') {
         alert('Ingrese una tarea')
      } else {
-    // if ( task !== '') {
-
         renderTask(task);
+        addGeneralButtons();
         clearEntry();
     };
 };
@@ -24,20 +23,22 @@ function addTask () {
 btnAdd.addEventListener('click',addTask)
 
 
-
+//funcion que renderice cada tarea y sus funciones
 function renderTask (element) {
 
-    //obtener el valor del input que ingresa el usuario
-    const task = document.querySelector('.inputTask').value;
     //crear una caja que contenga toda la pregunta, con sus botones
     const boxTask = document.createElement('div');
     boxTask.className = 'boxTask';
 
     const content = `
-        <button class='check' id='toggle'>select</button>
+        <input type="checkbox" name="checkbox" class="check" id="btnselect">
         <span class='textTask'>${element}</span>
-        <button class='delete'>delete</button>
+        <button class='delete'>x</button>
     `
+  /*   <input type="checkbox" name="my-checkbox" id="opt-in">
+  <label for="opt-in">Check me!</label> */
+
+
     boxTask.innerHTML = content;
     containerTasks.appendChild(boxTask);
 
@@ -52,11 +53,11 @@ function renderTask (element) {
 
     //funcion que checkee la tarea indicada
     function checkTask () {
-
         //identificar a quien selecciono
         const selectedTask = btnCheck.nextElementSibling;
+        
         //a ese que selecciono cambie estilo y rayelo
-        selectedTask.style.textDecoration = "line-through";   
+        selectedTask.style.textDecoration = "line-through";  
 
         //hacer que se checke y no se checkee revisar toggle
     };
@@ -69,28 +70,39 @@ function renderTask (element) {
        deleteSelec.remove(); 
     };
   
-    // if (/*ya se ingreso una tarea */)
-    const boxButtons = document.createElement('div');
-    boxButtons.className = 'boxButtons';
+    // if (/* containerTasks ya tiene mas hijos de 1 >1  */) {/*cree el div con los buttons*/}
+ 
+    // const boxButtons = document.createElement('div');
+    // boxButtons.className = 'boxButtons';
     
-    const generalButtons = `
-        <button class='deleteButton'>deleteAll</button>
-        <button class='selectButton'>selectAll</button>
-    `
-    boxButtons.innerHTML = generalButtons;
-    containerTasks.appendChild(boxButtons)
+    // const generalButtons = `
+    //     <button class='deleteButton'>deleteAll</button>
+    //     <button class='selectButton'>selectAll</button>
+    // `
+    // boxButtons.innerHTML = generalButtons;
+    // containerTasks.appendChild(boxButtons)
+    // const btnDelete = boxButtons.querySelector('deleteButton');
+    // const btnSelect = boxButtons.querySelector('selectButton');
 
- /*    function addGeneralButtons () {
+    // btnDelete.addEventListener('click',deleteAll);
+    // btnSelect.addEventListener('click', selectAll);
 
-
-        // const btnDelete = boxButtons.querySelector('deleteButton');
-        // const btnSelect = boxButtons.querySelector('selectButton');
-    
-        // btnDelete.addEventListener('click',deleteAll);
-        // btnSelect.addEventListener('click', selectAll);
-    }; */
 
 };
+
+function addGeneralButtons () {
+    // console.log(containerTasks)
+/*     if (containerTasks == 1) {
+        console.log('aparecen botones')
+    } else {
+
+        console.log('no aparecen')
+    } */
+}; 
+
+
+
+
 
 function clearEntry () {
 
